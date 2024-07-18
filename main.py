ea37e8e805e59951a137042678f60d86427cb80b
Vagrant User <vagrant@cleanUbuntuJenkins>
2024-07-18 17:58:21 +0300

    adding day of the week

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from datetime import datetime
 
 # print current time
 print("The date today is: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+
+# print day of the week
+day = datetime.now().strftime("%A")
+print("Today is: ", day)
+    
